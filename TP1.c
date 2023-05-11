05aacbcb034df288e0d37d073451e671a5f9726b
Uberg001 <uriel.eitan.aisenberg@gmail.com>
2023-05-11 16:23:20 -0300

    Cambios y programa de prueba

--- TP1.c
+++ TP1.c
@@ -1,36 +1,49 @@
 #include <stdio.h>
 
 //1: Declaracion de funciones
-int Lenght(){
+int lenght(char* cadena){
 
-} //a
+} //a:Calcular y retornar la longitud de una cadena dada.
 
-int toInt(){
+int toInt(char* cadena){
 
-} //b
+} //b:Convertir una cadena de dígitos en su equivalente numérico. 
 
-char* toUpper(){
+char* toUpper(char* cadena){
 
-} //c
+} //c:Convertir una cadena de caracteres a la misma cadena con todos sus caracteres en mayúscula. 
 
-void deleteChar(){
+char* deleteChar(char* cadena,char caracter){
 
-}//d
+}//d:Eliminar de una cadena dada todas las ocurrencias de un carácter dado. 
 
-char* concat(){
+char* concat(char* cadena1,char* cadena2){
 
-}//e
+}//e:Concatenar al final de la primera cadena dada una segunda cadena también dada.
 
-char* insertChar(){
+char* insertChar(char* cadena,char caracter,int pos){
 
-}//f
+}//f:Modificar la cadena dada con la inserción de un carácter dado en una posición determinada. 
 
 
 int main(){ //2: Testeo de funciones 
+    char cadena[]="Miguel Borja";
+    int longitud=lenght(cadena);//a
 
+    char cadenaNumeros[]="91218";
+    int num=toInt(cadenaNumeros);//b
 
+    char cadenaNueva[]=toUpper(cadena);//c
 
+    char cadenaMod[]=deleteChar(cadena,'u');//d
 
+    char cadena2[]="Gonzalo Martinez";
+    char cadena3[]=concat(cadena,cadena2);//e
+
+    char nuevaCadena[]=insertChar(cadena,'9',13);//f
+
+    //Impresion de las cadenas...
     
+
     return 0;
 }
