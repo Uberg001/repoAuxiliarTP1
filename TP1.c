9d623808f72e71a085a8c89f7505d00f58a64a45
Uberg001 <uriel.eitan.aisenberg@gmail.com>
2023-05-11 15:57:09 -0300

    Declaracion de funciones

--- TP1.c
+++ TP1.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 
-int main(){
-    
-    
+//1: Declaracion de funciones
+int Lenght(){
+
+} //a
+
+int toInt(){
+
+} //b
+
+char* toUpper(){
+
+} //c
+
+void deleteChar(){
+
+}//d
+
+char* concat(){
+
+}//e
+
+char* insertChar(){
+
+}//f
 
 
+int main(){ //2: Testeo de funciones 
+
+
+
+
+    
     return 0;
 }
